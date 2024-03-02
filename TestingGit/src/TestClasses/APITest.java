436b3a008b83aae9570411f1d7854e6642059258
ThivyaIBMer <thivyanagammaip@gmail.com>
2024-03-02 15:21:19 +0530

    first

---
+++ TestingGit/src/TestClasses/APITest.java
@@ -0,0 +1,9 @@
+package TestClasses;
+
+public class APITest {
+
+	public static void main(String[] args) {
+		System.out.println("Hi am in API");
+	}
+
+}
---
+++ TestingGit/src/TestClasses/Selenium.java
@@ -0,0 +1,11 @@
+package TestClasses;
+
+public class Selenium {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi am in Selenium");
+
+	}
+
+}
