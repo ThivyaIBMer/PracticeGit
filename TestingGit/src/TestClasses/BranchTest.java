f59f2e3866bd0e554faf8884b7a0219eb68224be
ThivyaIBMer <thivyanagammaip@gmail.com>
2024-03-05 11:34:56 +0530

    test

--- TestingGit/src/TestClasses/BranchTest.java
+++ TestingGit/src/TestClasses/BranchTest.java
@@ -5,8 +5,8 @@ public class BranchTest {
 	public static void main(String[] args) {
 		System.out.println("Branch Test");
 		int a=2;
-		int b=3;
-		if(a==b) {
+	
+		if(a==3) {
 			System.out.println("not equal");
 		}
 		else {
